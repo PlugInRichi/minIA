f362056a651e2d43a4f4a22a9cecb24cf29f010d
Ricardo G <rich030rich@gmail.com>
2020-03-16 20:33:52 -0600

    lectura_img
    Añade la función que recupera las rutas de la carpeta imagenes a el archivo de utilidades

--- minIA/utiles.py
+++ minIA/utiles.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+from os import path, scandir
 
-
-
-
+#Obtiene una lista con la ruta de cada archivo en el directorio
+#images del proyecto
+def lectura_img():
+    path_img = path.join(path.abspath('..'),'images')
+    if path.exists(path_img):
+        return [archivo.path for archivo in scandir(path_img)]
+    else:
+        print("La ruta especificada no existe")
+        return list()
