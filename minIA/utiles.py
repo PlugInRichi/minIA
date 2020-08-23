c69b98604b287e5c9fd692696960096fad903067
Ricardo G <rich030rich@gmail.com>
2020-08-23 02:17:49 -0500

    Utiles: función búsqueda
    Ahora recibe la ruta absoluta o relativa del directorio a recuperar imágenes

--- minIA/utiles.py
+++ minIA/utiles.py
@@ -3,12 +3,20 @@
 
 from os import path, scandir
 
-#Obtiene una lista con la ruta de cada archivo en el directorio especificado
+'''
+Obtiene una lista con la ruta de cada archivo en el directorio especificado,
+si la ruta no existe retornará una lista vacía. ¡El directorio de búsqueda NO
+debe contener subdirectorios!
+Parámetros:
+    Dirección absoluta o relativa de búsqueda
+'''
 def lectura_img(name_dir):
-    path_img = path.join(path.abspath('..'),'images\\'+ name_dir)
+    path_img = path.join(path.abspath('..'), name_dir)
     if path.exists(path_img):
         return [archivo.path for archivo in scandir(path_img)]
+    elif path.exists(name_dir):
+        return [archivo.path for archivo in scandir(name_dir)]
     else:
-        print("La ruta especificada no existe:")
-        print(path_img)
+        print("\nLa ruta especificada no existe:")
+        print(path_img+"\n")
         return list()
