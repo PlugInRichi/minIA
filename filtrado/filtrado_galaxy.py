85e24165b3b6eb4f17514ec756e20b490facc6ef
jocesacs <axel_castillo98@hotmail.com>
2021-06-29 21:08:28 -0500

    Script de filtrado terminado.

--- filtrado/filtrado_galaxy.py
+++ filtrado/filtrado_galaxy.py
@@ -18,22 +18,23 @@ import argparse
 
 #Argumentos
 parser = argparse.ArgumentParser(
-        description='''Aplica una mascara a cada una de las imagenes
-                    del directorio original y las guarda en el
-                    directorio destino''')
-parser.add_argument("th", help = "Threshold o umbral", type=int)
-parser.add_argument("kernel", help = "", type=int)
-parser.add_argument("dir_img", help = 'Ruta del directorio de imAgenes.')
-parser.add_argument("dir_output", help = 'Ruta del directorio de salida de imágenes con máscara.')
+        description='''Aplica una mascara a cada una de las imagenes del
+        directorio original y las guarda en el directorio destino''')
+parser.add_argument("th", 
+        help = "Threshold o umbral", type=int)
+parser.add_argument("kernel",
+        help="Tamaño del kernel del filtro de suavizado", type=int)
+parser.add_argument("dir_img",
+        help = 'Ruta del directorio de imágenes.')
+parser.add_argument("dir_output",
+        help = 'Ruta del directorio de salida de imágenes con máscara.')
 
 #Introduciendo todos los argumentos. 
 args = parser.parse_args()
 
 #Obtención de imágenes
 path_images = lectura_img(args.dir_img)
-print(path_images)
 
-nombre = 1
 #Ciclo de implementación de máscara
 for image in tqdm(path_images):
     #Leemos la imagen a color y después la convertimos a escala de grises
@@ -46,9 +47,9 @@ for image in tqdm(path_images):
     mask[mask > 0] = 1
     #Aplicación de la máscara
     img_filter = cv.bitwise_and(img_c, img_c, mask = mask)
-    #La siguiente línea está comentada porque invierte los colores.
-    plt.imsave(args.dir_output + "/" + str(nombre) + ".jpeg", img_filter)
-    #cv.imwrite(args.dir_output + "/" + str(nombre) + ".jpeg", img_filter)
-    #print("imagen " + str(nombre) + " terminada")
-    nombre+=1
-
+    #Extraemos el nombre de la img del path
+    base_nom = path.basename(image)
+    nom_img = path.splitext(base_nom)[0]
+    #Guardamos la imagen
+    cv.imwrite(args.dir_output + "/" + nom_img + ".jpeg", img_filter)
+    
