65bca3972e0fd6be65ad18805f9fb4bed3ba2eec
Ricardo García <rich030rich@gmail.com>
2021-10-04 18:17:52 -0500

    Variabilidad para las transformaciones en las imágenes

--- src/minIA/imageProcessing.py
+++ src/minIA/imageProcessing.py
@@ -1,3 +1,4 @@
+import random
 import cv2 as cv
 import numpy as np
 
@@ -14,12 +15,13 @@ def filterImg(image, kernel, th=6):
 def changePosition(image):
     height, width = image.shape[:2]
     center = (width / 2, height / 2)
-    quarter_height, quarter_width = height / 8, width / 8
+    eighth_height, eighth_width = height / 8, width / 8
+    sng = 1 if random.random() < 0.5 else -1
     T = np.float32([
-            [1, 0, quarter_width],
-            [0, 1, quarter_height]])
-    R = cv.getRotationMatrix2D(center, angle=30, scale=1)
+            [1, 0, sng * eighth_width],
+            [0, 1, sng * eighth_height]])
+    R = cv.getRotationMatrix2D(center, angle=sng*30, scale=1)
     img_translation = cv.warpAffine(image, T, (width, height))
     img_rotation = cv.warpAffine(image, R, (width, height))
-    img_flip = cv.flip(img_rotation, -1)
+    img_flip = cv.flip(img_rotation, sng)
     return img_flip
