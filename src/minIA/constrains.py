cb3ece48c49918e86fede769c409ddc3177dec91
ricardo <rich030rich@gmail.com>
2022-07-17 20:36:58 -0500

    Constants file creation.

---
+++ src/minIA/constrains.py
@@ -0,0 +1,60 @@
+DEBIASED = [
+    'asset_id',
+    't08_odd_feature_a19_ring_debiased',
+    't08_odd_feature_a20_lens_or_arc_debiased',
+    't08_odd_feature_a21_disturbed_debiased',
+    't08_odd_feature_a22_irregular_debiased',
+    't08_odd_feature_a24_merger_debiased',
+    't08_odd_feature_a38_dust_lane_debiased',
+
+    't11_arms_number_a31_1_debiased',
+    't11_arms_number_a32_2_debiased',
+    #'t11_arms_number_a33_3_debiased',
+    #'t11_arms_number_a34_4_debiased',
+
+    't05_bulge_prominence_a10_no_bulge_debiased',
+    't05_bulge_prominence_a11_just_noticeable_debiased',
+    't05_bulge_prominence_a12_obvious_debiased',
+    't05_bulge_prominence_a13_dominant_debiased',
+
+    't07_rounded_a16_completely_round_debiased',
+    't07_rounded_a17_in_between_debiased',
+    't07_rounded_a18_cigar_shaped_debiased',
+
+    't09_bulge_shape_a25_rounded_debiased',
+    't09_bulge_shape_a26_boxy_debiased',
+    't09_bulge_shape_a27_no_bulge_debiased',
+
+    't10_arms_winding_a28_tight_debiased',
+    't10_arms_winding_a29_medium_debiased',
+    't10_arms_winding_a30_loose_debiased']
+
+CLASS_NAMES = [
+    't08_ring',
+    't08_lens_or_arc',
+    't08_disturbed',
+    't08_irregular',
+    't08_merger',
+    't08_dust_lane',
+
+    't11_arms_number_1',
+    't11_arms_number_2',
+    #'t11_arms_number_3',
+    #'t11_arms_number_4',
+
+    't05_no_bulge',
+    't05_just_noticeable',
+    't05_obvious',
+    't05_dominant',
+
+    't07_completely_round',
+    't07_in_between',
+    't07_cigar_shaped',
+
+    't09_rounded',
+    't09_boxy',
+    't09_no_bulge',
+
+    't10_tight',
+    't10_medium',
+    't10_loose']
