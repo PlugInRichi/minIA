97ebaadaffe91f8756c77b3acb6f2fa0baf8aa28
ricardo <rich030rich@gmail.com>
2022-08-22 21:52:53 -0500

    Script to create and download files

---
+++ install/configure.sh
@@ -0,0 +1,15 @@
+
+cd ../data
+mkdir "tf_records"
+mkdir "models"
+mkdir -p "images"
+
+wget -O images_gz2.zip https://zenodo.org/record/3565489/files/images_gz2.zip?download=1
+zip -d images_gz2.zip "__MACOSX"
+unzip -q -j images_gz2.zip -d images/images_gz2
+rm images_gz2.zip
+
+cd models
+curl -Os http://storage.googleapis.com/delf/resnet50_imagenet_weights.tar.gz
+tar -xzvf resnet50_imagenet_weights.tar.gz
+rm resnet50_imagenet_weights.tar.gz
