b037009c63c438ec81ffb24d9d59225439d64d2f
ricardo <rich030rich@gmail.com>
2022-09-05 00:59:23 -0500

    Test delf model

---
+++ src/delf/delf_extractor.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+
+from google.protobuf import text_format
+from delf import delf_config_pb2
+from delf import utils
+from utils import custom_extractor as extractor
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+class ExtractorDELF:
+
+    def __init__(self, config_path):
+        config = delf_config_pb2.DelfConfig()
+        with tf.io.gfile.GFile(config_path, 'r') as f:
+            text_format.Merge(f.read(), config)
+        self.model = extractor.MakeExtractor(config)
+
+    def extract_features(self, image_path):
+        # Extract features.
+        im = np.array(utils.RgbLoader(image_path))
+        extracted_features = self.model(im)
+        return  extracted_features['local_features']
+
+if __name__ == '__main__':
+    e = ExtractorDELF('/data/config/delf_config_galaxy.pbtxt')
+    print(e.extract_features('/data/images/images_gz2/1022.jpg'))
